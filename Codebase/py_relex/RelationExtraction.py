3ef82a555345ea68d7a448be8cd30dd599b6f2e1
Anisha Jauhari <anishajauhari10@gmail.com>
2022-10-25 19:12:43 -0500

    Codebase

--- Codebase/py_relex/RelationExtraction.py
+++
@@ -1,5 +0,0 @@
-class RelationExtraction:
-    def __init__(self, tagged_tokens, gt_relations, negative_flag):
-        self.tagged_tokens = tagged_tokens
-        self.gt_relations = gt_relations
-        self.negative_flag = negative_flag
